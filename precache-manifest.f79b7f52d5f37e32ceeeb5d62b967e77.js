7ec9b6de48b348ea57703460132dd93b982715f3
Mega Putra <megahsputra@gmail.com>
2021-04-11 15:29:23 -0700

    Updates

---
+++ precache-manifest.f79b7f52d5f37e32ceeeb5d62b967e77.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "055edb63e3bf4ceefcb4",
+    "url": "/algolia-se-assessment/static/css/main.f2caf18c.chunk.css"
+  },
+  {
+    "revision": "055edb63e3bf4ceefcb4",
+    "url": "/algolia-se-assessment/static/js/main.055edb63.chunk.js"
+  },
+  {
+    "revision": "1ba4736647d2e169e113",
+    "url": "/algolia-se-assessment/static/js/1.1ba47366.chunk.js"
+  },
+  {
+    "revision": "ab0ba178056df2a8e7f0",
+    "url": "/algolia-se-assessment/static/js/runtime~main.ab0ba178.js"
+  },
+  {
+    "revision": "4de30c41f4918d0a47257e3525fbfb49",
+    "url": "/algolia-se-assessment/index.html"
+  }
+];
